7c49bba0fe09af3998d32874e31e9041a488d12e
Marc España Quinquilla <meq.1515@gmail.com>
2024-11-12 09:50:52 +0100

    First part finished

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,20 +1,18 @@
 import type { PropsWithChildren } from "react";
-import Header from "./header";
+import { Header } from "./header";
 
-const Layout = ({ children }: PropsWithChildren) => {
+export function Layout({ children }: PropsWithChildren) {
   return (
-    <div className="bg-gradient-to-br from-background to-muted">
+    <div className=" bg-gradient-to-br from-background to-muted">
       <Header />
       <main className="min-h-screen container mx-auto px-4 py-8">
         {children}
       </main>
-      <footer className="border-t backdrop-blur py-12 supports-[backdrop-filer]:bg-background/60">
-        <div className="container mx-auto px-4 text-center text-gray-400">
-          Made by marcespana.com
+      <footer className="border-t backdrop-blur supports-[backdrop-filter]:bg-background/60 py-12">
+        <div className="container mx-auto px-4 text-center text-gray-200">
+          <p>Made by marcespana.com</p>
         </div>
       </footer>
     </div>
   );
-};
-
-export default Layout;
+}
